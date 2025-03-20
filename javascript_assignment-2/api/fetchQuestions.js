99743d2bf05d8b7e1c30dfc519da83f78faa1ed1
Ashish Dode <ashish.05dode@gmail.com>
2025-03-20 12:46:27 +0530

    Updated fetchQuestions.js to fix CORS issue

--- javascript_assignment-2/api/fetchQuestions.js
+++ javascript_assignment-2/api/fetchQuestions.js
@@ -1,5 +1,5 @@
 async function fetchQuestions(category, difficulty) {
-    const url = `https://opentdb.com/api.php?amount=20&category=${category}&difficulty=${difficulty}&type=multiple`;
+    const url = `https://api.allorigins.win/raw?url=${encodeURIComponent(`https://opentdb.com/api.php?amount=20&category=${category}&difficulty=${difficulty}&type=multiple`)}`;
     const response = await fetch(url);
     const data = await response.json();
     return data.results;
